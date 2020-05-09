a56ee8e0509fb1a50fb9e0f340a49c8c7b6aec0d
Tugume Andrew <andrewtugume2@gmail.com>
2020-05-10 00:11:08 +0300

    completed authentication

--- js/login.js
+++ js/login.js
@@ -1,61 +1,64 @@
-const baseUrl = 'https://pelard-n.herokuapp.com';
-const secret = '2cfb9e9a-34a9-4843-961f-6e2639c41856-b10445eb-a0e8-4fa2-b636-015b2f1e3660';
 
-
-const getToken = async ({ secret, _id }) => {
-  const response = await fetch(`${baseUrl}/token/generate`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ secret, _id })
-  });
-
-  const json = await response.json();
-  console.log(json);
-   sessionStorage.setItem('access', json.data.token)
-  return json.data.token;
-};
-
-async function isLoggedIn () {  
-const access= sessionStorage.getItem('access')
-if (!access) return false
-}
-
-const userLogin = async () => {
-const userName =document.getElementById('userName').value;
-const password =document.getElementById('password').value;
-  try {
-    const token = await getToken({ secret });
-    const validLogin = await isLoggedIn()
-
-//async function autoRedirect () {}
-    const response = await fetch(`${baseUrl}/user/login`, {
+  const baseUrl = 'https://pelard-n.herokuapp.com';
+  const secret = '2cfb9e9a-34a9-4843-961f-6e2639c41856-b10445eb-a0e8-4fa2-b636-015b2f1e3660';
+  
+  
+  const getToken = async ({ secret, _id }) => {
+    const response = await fetch(`${baseUrl}/token/generate`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: token
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(
-        {
-      userName: userName,
-      password: password
-        }
-        )
+      body: JSON.stringify({ secret, _id })
     });
-
+  
     const json = await response.json();
-
-console.log('Logged In');
-if (!validLogin && location.pathname !== '/index' || '/') window.location = '/index.html';
-if (validLogin && location.pathname === '/index' || '/') window.location ='/report.html';
-     
-  } catch (errors) {
-    console.log(errors);
-  }
-
-
+    console.log(json);
+     sessionStorage.setItem('access', json.data.token)
+    return json.data.token;
+  };
+  
+  async function isLoggedIn () {  
+  const access= sessionStorage.getItem('access')
+  if (!access) return false
 }
 
+  const userLogin = async () => {
+  const userName =document.getElementById('userName').value;
+  const password =document.getElementById('password').value;
+    try {
+      const token = await getToken({ secret });
+      const validLogin = await isLoggedIn()
 
-document.getElementById('create-group-button').addEventListener('click',userLogin);
+//async function autoRedirect () {}
+      const response = await fetch(`${baseUrl}/user/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        },
+        body: JSON.stringify(
+          {
+        userName: userName,
+        password: password
+          }
+          )
+      });
+  
+      const json = await response.json();
+      console.log(json);
+  console.log('Logged In');
+  sessionStorage.setItem('_id', json.data.user._id)
+  if (!validLogin && location.pathname !== '/index') window.location = '/report.html';
+  if (validLogin && location.pathname === '/index') window.location ='/index.html';
+       
+    } catch (errors) {
+      console.log(errors);
+    }
+
+  
+  }
+  
+ 
+  document.getElementById('create-group-button').addEventListener('click',userLogin);
+
---
+++ js/report.js
@@ -0,0 +1,117 @@
+const baseUrl = 'https://pelard-n.herokuapp.com';
+  const secret = '2cfb9e9a-34a9-4843-961f-6e2639c41856-b10445eb-a0e8-4fa2-b636-015b2f1e3660';
+  const _id= sessionStorage.getItem('_id')
+  
+  const getToken = async ({ secret, _id }) => {
+    const response = await fetch(`${baseUrl}/token/generate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ secret, _id })
+    });
+  
+    const json = await response.json();
+    console.log(json);
+     sessionStorage.setItem('access', json.data.token)
+     return json.data.token;
+  };
+  
+  async function isLoggedIn () {  
+  const access= sessionStorage.getItem('access')
+  if (!access) return false
+}
+
+  const userReport = async () => {
+     //Reporters Information
+		const reporterName = document.getElementById("reporterName").value;
+		const reporterContact = document.getElementById("reporterContact").value;
+		const dateTime =document.getElementById('dateTime').value;
+  //Violation Details
+		const districtOfViolation = document.getElementById("districtOfViolation").options[
+							  document.getElementById("districtOfViolation").selectedIndex].value;
+		 const violationType = document.getElementById("violationType").options[
+							  document.getElementById("violationType").selectedIndex].value;
+		const violationDescription = document.getElementById("violationDescription").value;
+		//const injuries = document.getElementById("injuries").value;
+		// const contactAuthority = document.getElementById("contactAuthority").value;
+		//const authorityResponse = document.getElementById("authorityResponse").value;
+		//const otherViolation = document.getElementById("otherViolation").value;
+  //People Involved 
+		const victimName = document.getElementById("victimName").value;
+		const otherVictim = document.getElementById("otherVictim").value;
+		const suspectName = document.getElementById("suspectName").value;
+		const otherSuspect  = document.getElementById("otherSuspect").value;
+		const witnessName = document.getElementById("witnessName").value;
+		const otherWitness = document.getElementById("otherWitness").value;
+  //Evidence     
+		//const files = document.getElementById("img").files;
+		//const fileDescription = document.getElementById("fileDescription").value;
+    try {
+      const token = await getToken({ secret, _id });
+      const validLogin = await isLoggedIn()
+
+//async function autoRedirect () {}
+const response = await fetch(`${baseUrl}/violations/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        },
+        body: JSON.stringify(
+            {
+                   type: violationType,
+                   dateTime: dateTime,
+                   description: violationDescription,
+                   reporter: {
+                       name: reporterName,
+                       contact: reporterContact,
+                   },
+                   location: {
+                    name: districtOfViolation,
+                  },
+                  involved: [
+                    { type: "victim", name: victimName,
+                    relevantLinks:[
+                    { 
+                      description:otherVictim,
+                      link: "string"
+                      }
+                    ] 
+                    },
+        
+                    { type: "suspect", name: suspectName,
+                    relevantLinks:[
+                    { 
+                      description:otherSuspect,
+                      link: "string"
+                      }
+                    ] 
+                     },
+                    { type: "witness", name: witnessName,
+                    relevantLinks:[
+                    { 
+                      description:otherWitness,
+                      link: "string"
+                      }
+                    ] 
+                     },
+                  ]
+               })
+      });
+  
+      const json = await response.json();
+      console.log(json);
+  console.log('Case Documented');
+ if (!validLogin && location.pathname !== '/report') window.location = '/report.html';
+ if (validLogin && location.pathname === '/report') window.location ='/submitted.html';
+       
+    } catch (errors) {
+      console.log(errors);
+    }
+
+  
+  }
+  
+ 
+  document.getElementById('create-group-button').addEventListener('click',userReport);
