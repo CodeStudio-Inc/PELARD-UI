fbb56810e83815846f01f494668e27e1d08128f1
Tugume Andrew <andrewtugume2@gmail.com>
2020-05-25 23:38:54 +0300

    added manifest file

--- sw.js
+++ sw.js
@@ -30,23 +30,85 @@ const resourcesToPrecache=[
 'images/sol.png'
 ];
 
-
-self.addEventListener('install',event =>{
-    console.log('Service worker install event!');
-    event.waitUntil(
-            caches.open(cacheName)
-            .then(cache =>{
-                return cache.addAll(resourcesToPrecache);
-            })
-    );
+self.addEventListener("install", async (event) => {
+  console.log('Service worker installing!');
+//”caches” is an instance of the CacheStorage
+//the method “open” returns a promise that resolves to 
+//the cache object matching the cache name
+  const cache = await caches.open(cacheName);
+  await cache.addAll(resourcesToPrecache);
+  //allow the newly installed service worker to move on to activation
+  return self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
+// self.addEventListener('install',event =>{
+//     console.log('Service worker install event!');
+//     event.waitUntil(
+//             caches.open(cacheName)
+//             .then(cache =>{
+//                 return cache.addAll(resourcesToPrecache);
+//             })
+//     );
+// });
 
-    console.log('sw activated');
 
-});
+self.addEventListener("activate", event => {
+  self.clients.claim();
+  console.log('sw activated');
+}); 
 
+// self.addEventListener('activate', function(event) {
+
+//     console.log('sw activated');
+
+// });
+
+
+
+// //serviceWorker.js
+// self.addEventListener("fetch", async event => {
+//   const req = event.request;
+//   const url = new URL(req.url);
+// //check if the request is requiring data from our own application(location)
+//   if (url.origin === location.origin) {
+// //check our cache
+//     event.respondWith(checkCache(req));
+//   } 
+// //else, fetch from the network and cache that result
+//   else {
+//     event.respondWith(checkNetwork(req));
+//   }
+// });
+
+// async function checkCache(req) {
+// //open our cache
+//   const cache = await caches.open(cacheName);
+// //check if there’s data there that match with what the request requires
+//   const cachedData = await cache.match(req);
+// //if there’s data cached, return it, else fetch from the network
+//   return cachedData || fetch(req);
+// }
+
+// async function checkNetwork(req) {
+// //open our cache
+//   const cache = await caches.open(cacheName);
+// //try to fetch data from the network
+//   try {
+// //save the fetched data
+//     const freshData = await fetch(req);
+// //save a copy of the response to your cache
+//     await cache.put(req, freshData.clone());
+// //send the response back (returned the fetched data)
+//     return freshData;
+//   } 
+// //if we are unable to fetch from the network (offline)
+//   catch (err) {
+// //match the request with data from the cache
+//     const cachedData = await cache.match(req);
+// //return the cached data
+//     return cachedData;
+//   }
+// }
 self.addEventListener('fetch', event => {
     event.respondWith(
       caches.open(cacheName)
@@ -57,3 +119,4 @@ self.addEventListener('fetch', event => {
     );
   });
   
+ 
