3791275008638372229e1fa704d3e44ab5e1412e
tugumeandree <andrewtugume2@gmail.com>
2021-12-20 14:39:43 +0300

    update

--- js/report.js
+++ js/report.js
@@ -167,12 +167,12 @@ const response = await fetch(`${baseUrl}/violations/create`, {
       const json = await response.json();
       console.log(json);
   if (!validLogin && location.pathname !== '/pages/report'){
-    // window.location ='/pages/submitted.html' ;
-    console.log('Case Not Documented');
+    window.location ='/pages/submitted.html' ;
+    console.log('Case Documented');
     document.getElementById("error").innerHTML = `<span style='font-size:40px; color:red;'>please fill all the inputs</span>`;
   } 
   if (validLogin && location.pathname === '/pages/report'){
-    console.log('Case Documented');
+    console.log('Case Not Documented');
     // window.location ='/pages/submitted.html';
   } 
        
